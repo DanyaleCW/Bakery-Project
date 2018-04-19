9bb97807fa1c4d100a7bb7b3a10813e1637a84a9
Danyale <danyalecw@gmail.com>
2018-04-18 22:06:46 -0400

    add css and html

--- items.rb
+++ items.rb
@@ -1,26 +1,34 @@
 class BakeryItems
     attr_accessor :name, :description, :price
 
-        def initalize(name, description, price)
+        def initialize(name, description, price)
             @name = name
             @description = description
             @price = price
-    end 
+        end 
 end         
 
         class Cookie < BakeryItems
-            
+            def initialize(name, description, price)
+                super
+            end
         end    
 
         class Cake < BakeryItems
-            
+            def initialize(name, description, price)
+                super
+            end
         end    
 
         class Muffin < BakeryItems
-            
+            def initialize(name, description, price)
+                super
+            end
         end                   
 
 cookie = Cookie.new("Double Chocolate Chunk", "The best Double Chocolate Chip cookie you will ever try.", 2.50)
+
+
 cake = Cake.new("Chocolate Devil", "The best Double Chocolate Chip cake you will ever try.", 6.00)
 muffin = Muffin.new("Double Chocolate Chip", "The best Double Chocolate Chip muffin you will ever try.", 3.00)
 
